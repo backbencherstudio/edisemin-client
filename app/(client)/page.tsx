624c6a66ba6db18ea01b682e00a14db93c960e9d
tushar <tushar789imran@gmail.com>
2025-06-03 14:49:30 +0600

    journy part done

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./_components/banner";
+import Journy from "./_components/journy";
 
 export default function HomePage() {
   return (
     <div>
       <Banner />
+      <Journy />
     </div>
   );
 }
