092e1fb306420b0832948c30466943519df3b06d
tushar-h789 <tushar789imran@gmail.com>
2025-06-04 13:04:47 +0600

    student career section done

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -3,6 +3,7 @@ import Banner from "./_components/banner";
 import Journy from "./_components/journy";
 import CareerPath from "./_components/career-path";
 import CareerCalculator from "./_components/career-calculator";
+import StudentCareer from "./_components/student-career";
 
 export default function HomePage() {
   return (
@@ -11,6 +12,7 @@ export default function HomePage() {
       <Journy />
       <CareerPath />
       <CareerCalculator />
+      <StudentCareer />
     </div>
   );
 }
